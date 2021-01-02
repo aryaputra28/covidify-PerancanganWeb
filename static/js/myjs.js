56ceb53924a1d7863d9f2dcc98ab8d93a1f2f3e5
gilangcy <gilangcaturyudistira@gmail.com>
2021-01-02 17:19:59 +0700

    pushtest

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -3,6 +3,4 @@ $(document).ready(function(){
     // $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'dd--mm-yy'});
     $(".datepicker").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
     $(".datepicker").attr("autocomplete", "off");
-   
-   
   });
